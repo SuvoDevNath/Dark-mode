cace55832098473cfe8622c49578b3cd5a6870ed
Suvo <89963235+SuvoDevNath@users.noreply.github.com>
2024-09-26 13:56:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// const icon=document.getElementById("icon");
+// const body=document.getElementById("body");
+
+// icon.addEventListener("click",()=>{
+//     body.classList.add.toggle("dark-theme")
+
+// })
+
+
+
